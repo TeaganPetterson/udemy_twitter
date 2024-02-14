c814c563a4b310cbd93dce9941d2e01d4593d0e5
Teagan Petterson <teagan.petterson1@gmail.com>
2024-02-14 12:44:12 -0700

    initial db migration

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen items-center justify-center">
-			<div className="flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-300 rounded-lg">
-				<div className="text-center my-4">
-					<h1>Strings</h1>
-				</div>
-				<div>
-					<Link href="/signup" className="bg-slate-400 my-4 p-3 rounded-lg block">Sign Up</Link>
-				</div>
-				<div>
-					<Link href="/signin" className="bg-slate-400 my-4 p-3 rounded-lg block">Sign In</Link>
-				</div>
-			</div>
-		</main>
-	)
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <div className="flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-300 rounded-lg">
+                <div className="text-center my-4">
+                    <h1>Strings</h1>
+                </div>
+                <div>
+                    <Link
+                        href="/signup"
+                        className="bg-slate-400 my-4 p-3 rounded-lg block"
+                    >
+                        Sign Up
+                    </Link>
+                </div>
+                <div>
+                    <Link
+                        href="/signin"
+                        className="bg-slate-400 my-4 p-3 rounded-lg block"
+                    >
+                        Sign In
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
 }
